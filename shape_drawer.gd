a724a92d8efe9f8ebfeb8a7d18a9f0cb09f9f2b8
pir0j0c232323 <pir0j0c232323@yandex.ru>
2026-08-12 11:53:21 +0300

    feat(drawer): пунктир, жирный контур призрака, настройки вынесены в переменные

--- shape_drawer.gd
+++ shape_drawer.gd
@@ -8,7 +8,8 @@ var first_point: Vector2 = Vector2.ZERO
 var current_point: Vector2 = Vector2.ZERO
 var defolt_punctir_color: Color = Color.AZURE
 var defolt_punctir_tolshina: float = 2.0
-var defolt_prizrac_color: Color = Color(1, 1, 1, 0.6)
+var defolt_prizrac_color: Color = Color(1, 1, 1, 0.3)
+var defolt_prizrak_tolshina = 5
 
 @onready var spawner = $"../ObjectSpawner"
 
@@ -57,13 +58,18 @@ func _draw():
 	print(" draw вызван")
 	if draw_tool == 0:
 		var radius = first_point.distance_to(current_point)
-		draw_line(first_point, current_point, defolt_punctir_color, defolt_punctir_tolshina)
-		draw_arc(first_point,radius, 0, TAU, 48, defolt_prizrac_color, 1)
+		var napravlenie = (current_point - first_point).normalized()
+		var t = 0.0
+		while  t < radius:
+			var nachalo = first_point + napravlenie*t
+			var konec = first_point + napravlenie*min(t+10.0, radius)
+			draw_line(nachalo, konec, defolt_punctir_color, defolt_punctir_tolshina)
+			t += 10.0 + 6.0
 		var points = PackedVector2Array()
 		for i in range(48):
 			var angle = i / 48.0 * TAU
 			points.append(first_point + Vector2(cos(angle), sin(angle))*radius)
 		draw_colored_polygon(points, defolt_prizrac_color)         
-		
+		draw_arc(first_point,radius, 0, TAU, 48, defolt_prizrac_color, defolt_prizrak_tolshina)
 		
 		
